50122389ace14d11e4dbcd0c06d92e37159a0673
Oumaima_rhezzoune <ouma.rhezzoune@gmail.com>
2023-07-29 20:41:00 +0200

    Add backend API service & controller

--- interview-backend/src/app.controller.ts
+++ interview-backend/src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly filePath: string;
+  constructor(private readonly appService: AppService) {
+    this.filePath = process.env.JSON_FILE_PATH || './cities.json';
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('data')
+  getData() {
+    const jsonData = this.appService.readJsonFile(this.filePath);
+    return jsonData;
+  }
+  @Get('search/:cityName')
+  searchCity(@Param('cityName') cityName: string) {
+    const jsonData = this.appService.readJsonFile(this.filePath);
+    
+    if (!jsonData) {
+      throw new NotFoundException('JSON data not available.');
+    }
+
+    const city = jsonData.find((city) => city.cityName.toLowerCase() == cityName.toLowerCase());
+
+    if (!city) {
+      throw new NotFoundException('City not found.');
+    }
+
+    return city;
   }
 }
--- interview-backend/src/app.service.ts
+++ interview-backend/src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  readJsonFile(filePath: string): any {
+    try {
+      const rawData = fs.readFileSync(filePath, 'utf8');
+      return JSON.parse(rawData);
+    } catch (error) {
+      throw new NotFoundException('JSON file not found or could not be read.');
+    }
   }
 }
