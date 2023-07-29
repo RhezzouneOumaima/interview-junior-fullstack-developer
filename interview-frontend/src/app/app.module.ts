f7f39fa116a58a26b01fad1ee5b61d8c11dbfa7e
Oumaima_rhezzoune <ouma.rhezzoune@gmail.com>
2023-07-29 22:40:21 +0200

    Clean code

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CitySearchFormComponent } from './city-search-form/city-search-form.component';
-import { CityResultsListComponent } from './city-results-list/city-results-list.component';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { HttpClientModule } from '@angular/common/http';
 
@@ -12,8 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    CitySearchFormComponent,
-    CityResultsListComponent
   ],
   imports: [
     BrowserModule,
